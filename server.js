6aa6a57a6255b0776d2ac97ec1e3261acc13bf79
Yangz <yangz@yangzblog.com>
2018-03-13 20:53:43 +0800

    optimize

--- server.js
+++ server.js
@@ -1,66 +1,76 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-const path=require('path');
+const path = require('path');
 const router = require('./router');
 const mime = require('./common/mime');
 const config = require('./config');
+const _ = require('underscore');
 
 const port = config.port;
 const assetsPath = config.assetsPath;
 
-const sendNotFound = function (res) {
-    res.writeHead(404, {
+const sendError = function (res, err) {
+    res.writeHead(500, {
         'Content-Type': mime.html
     });
-    res.write('<html><head><title>404</title></head><body><h1>404 NOT FOUND</h1></body></html>');
+    res.write(err.toString());
     res.end();
+}
+
+const sendNotFound = function (res) {
+    fs.readFile(config.templatePath + '/404.ejs', (err, data) => {
+
+        if (err) {
+            return sendError(res, err);
+        }
+
+        res.writeHead(404, {
+            'Content-Type': mime.html
+        });
+        res.write(data);
+        res.end();
+
+    });
 };
 
-const reg=/(\/[a-zA-Z0-9]+\/?)+:[a-zA-Z0-9]+/g;
-let newRouter={};
-for(let route in router){
-    reg.lastIndex=0;
-    if(router.hasOwnProperty(route)){
-        let newKey=route,
-            flag=false;
-        if(reg.test(route)){
-            newKey = route.split(':')[0] + '[a-zA-Z0-9]+[^\/]';
+const reg = /(\/[\S]+\/?)+:[\S]+/g;
+let newRouter = {};
+//转化路由规则
+for (let route in router) {
+    reg.lastIndex = 0;
+    if (router.hasOwnProperty(route)) {
+        let newKey = route,
+            flag = false;
+        if (reg.test(route)) {
+            newKey = route.split(':')[0] + '[\\S]+[^\/]';
             flag = true;
         }
-        newRouter['^'+newKey.replace(/\//g,'\\/')+'$']={
-            flag:flag,
-            handler:router[route]
+        newRouter['^' + newKey.replace(/\//g, '\\/') + '$'] = {
+            flag: flag,
+            handler: router[route]
         };
     }
 }
 
 const server = http.createServer((req, res) => {
-    const reqUrl = req.url;
-    const pathname = url.parse(reqUrl).pathname;
-    let flag=false;
-    //搜索匹配路由
-    for(let route in newRouter){
-        if(newRouter.hasOwnProperty(route)){
-            let reg=new RegExp(route);
-            if(pathname.match(reg)){
-                console.log('match');
-                let handler = newRouter[route].handler;
-                let param;
-                if(newRouter[route].flag){
-                    param=pathname.substr(pathname.lastIndexOf('/')+1)
-                }
-                typeof handler === 'function' && handler(req, res,param);
-                flag=true;
-                break;
-            }
-        }
-    }
+    const reqUrl = req.url,
+        pathname = url.parse(reqUrl).pathname;
 
-    if (!flag){
-        let pathname=url.parse(reqUrl).pathname;
-        let ext = path.parse(pathname).ext;
-        let realPath = assetsPath + pathname;
+    //搜索匹配路由
+    let matchedRouter = _.find(newRouter, function (routeObj, route) {
+        let reg = new RegExp(route);
+        return pathname.match(reg);
+    });
+
+    if (matchedRouter) {
+        let handler = matchedRouter.handler,
+            param = matchedRouter.flag ? pathname.substr(pathname.lastIndexOf('/') + 1) : undefined;
+        typeof handler === 'function' && handler(req, res, param);
+    } else {
+        let pathname = url.parse(reqUrl).pathname,
+            ext = path.parse(pathname).ext,
+            realPath = assetsPath + pathname;
         fs.stat(realPath, (err, stat) => {
             if (!err) {
                 if (stat.isFile()) {
